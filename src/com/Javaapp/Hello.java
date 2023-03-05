5490c49393fc2e90f037f24806612904436f0882
Lindow <Lindow@com.du>
2023-03-05 15:51:20 +0800

    master commit1

--- src/com/Javaapp/Hello.java
+++ src/com/Javaapp/Hello.java
@@ -2,9 +2,6 @@ package com.Javaapp;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello,world2");
-        System.out.println("hello,world3");
-        String s = new String("123");
-
+        System.out.println(System.currentTimeMillis());
     }
 }
