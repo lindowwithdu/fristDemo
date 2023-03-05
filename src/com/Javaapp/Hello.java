3d8a3f910fa434622388bd6d2a816dcf960f3333
Lindow <Lindow@com.du>
2023-03-05 16:16:59 +0800

    hot-fix commit2

--- src/com/Javaapp/Hello.java
+++ src/com/Javaapp/Hello.java
@@ -2,10 +2,6 @@ package com.Javaapp;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello,world2");
-        System.out.println("hello,world3");
-        String s = new String("123");
-
         User user = new User();
         user.setName("张三");
         user.setPassword("123");
