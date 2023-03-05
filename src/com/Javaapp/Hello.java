009f354d77975e3c6300323f25f0eafe681b9e36
lindowwithdu <126996976+lindowwithdu@users.noreply.github.com>
2023-03-05 16:28:10 +0800

    Update Hello.java

--- src/com/Javaapp/Hello.java
+++ src/com/Javaapp/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println(System.currentTimeMillis());
         System.out.println(new User());
+        System.out.println("厉不厉害你坤哥");
     }
 }
