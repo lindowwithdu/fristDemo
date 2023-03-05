d49e8434c57aab3738958e5a28429e7324913d7e
Lindow <Lindow@com.du>
2023-03-05 15:08:40 +0800

    second commit

--- src/com/Javaapp/Hello.java
+++ src/com/Javaapp/Hello.java
@@ -2,7 +2,6 @@ package com.Javaapp;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello,world1");
         System.out.println("hello,world2");
         System.out.println("hello,world3");
         String s = new String("123");
