969f7c1d708d8d91e77ee806526400cf1864d0c8
Lindow <Lindow@com.du>
2023-03-05 11:47:09 +0800

    first commit

---
+++ src/com/Javaapp/Hello.java
@@ -0,0 +1,11 @@
+package com.Javaapp;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello,world1");
+        System.out.println("hello,world2");
+        System.out.println("hello,world3");
+        String s = new String("123");
+
+    }
+}
---
+++ src/com/Javaapp/two.java
@@ -0,0 +1,23 @@
+package com.Javaapp;
+
+public class two {
+    private String name;
+    private String password;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "two{" +
+                "name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
